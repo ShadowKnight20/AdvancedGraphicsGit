c32ee29e666bf35f3971fa041a2be72965e1c422
ShadowKnight20 <48155299+ShadowKnight20@users.noreply.github.com>
2024-12-26 13:53:22 +0000

    addition to scene

--- SeaSerpent/Intermediate/PipInstall/Scripts/activate.bat
+++ SeaSerpent/Intermediate/PipInstall/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Unreal Engine Projects\SeaSerpent\SeaSerpent\Intermediate\PipInstall
+set VIRTUAL_ENV=C:\Users\Student\Documents\Module Dev Containers\AdvancedGraphicsGit\SeaSerpent\Intermediate\PipInstall
 
 if not defined PROMPT set PROMPT=$P$G
 
